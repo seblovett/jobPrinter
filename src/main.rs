a35b9026dfbc9656ad6d50aa0b0a0e98523e2541
seblovett <seblovett@googlemail.com>
2025-07-25 14:36:34 +0100

    Added POST API request to print jobs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,26 @@ use escpos::errors::Result as PrintResult;
 use escpos::printer::Printer;
 use escpos::utils::*;
 use serde::{Deserialize, Serialize};
-use std::fs;
+use std::{
+    fs,
+    error::Error,
+    net::Ipv4Addr,
+};
+
+use actix_web::{
+    middleware::Logger,
+    web::Data,
+    App, HttpServer,
+};
+use utoipa::{
+    OpenApi,
+};
+use utoipa_actix_web::AppExt;
+use utoipa_swagger_ui::SwaggerUi;
+
+use crate::todo::TodoStore;
+
+mod todo;
 
 #[derive(Serialize, Deserialize)]
 struct JobInfo {
@@ -68,10 +87,31 @@ fn print_from_file(file: &str) ->  Result<(), Box<dyn std::error::Error>>{
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[actix_web::main]
+async fn main() -> Result<(), impl Error> {
     env_logger::init();
-    // lsusb();
-    print_from_file("jobs.json")?;
-    Ok(())
-   
+
+    #[derive(OpenApi)]
+    #[openapi(
+        tags(
+            (name = "todo", description = "Todo management endpoints.")
+        ),
+    )]
+    struct ApiDoc;
+
+    let store = Data::new(TodoStore::default());
+        HttpServer::new(move || {
+        App::new()
+            .into_utoipa_app()
+            .openapi(ApiDoc::openapi())
+            .map(|app| app.wrap(Logger::default()))
+            .service(utoipa_actix_web::scope("/api/todo").configure(todo::configure(store.clone())))
+            .openapi_service(|api| {
+                SwaggerUi::new("/swagger-ui/{_:.*}").url("/api-docs/openapi.json", api)
+            })
+            .into_app()
+    })
+    .bind((Ipv4Addr::UNSPECIFIED, 8080))?
+    .run()
+    .await
 }
---
+++ src/todo.rs
@@ -0,0 +1,89 @@
+use std::sync::Mutex;
+
+use actix_web::{
+    post,
+    web::{Data, Json},
+    HttpResponse, Responder,
+};
+use serde::{Deserialize, Serialize};
+use utoipa::{ToSchema};
+use utoipa_actix_web::service_config::ServiceConfig;
+
+
+#[derive(Default)]
+pub(super) struct TodoStore {
+    todos: Mutex<Vec<Todo>>,
+}
+use escpos::driver::*;
+use escpos::errors::Result as PrintResult;
+use escpos::printer::Printer;
+use escpos::utils::*;
+
+const TODO: &str = "todo";
+
+pub(super) fn configure(store: Data<TodoStore>) -> impl FnOnce(&mut ServiceConfig) {
+    |config: &mut ServiceConfig| {
+        config
+            .app_data(store)
+            .service(create_todo);
+    }
+}
+
+ fn print(job: Todo) -> PrintResult<()> {
+    let driver = UsbDriver::open(0x0416, 0x5011, None, None)?;
+    Printer::new(driver, Protocol::default(), None)
+        .debug_mode(Some(DebugMode::Dec))
+        .init()?
+        .justify(JustifyMode::CENTER)?
+        .size(2, 2)?
+        .underline(UnderlineMode::Single)?
+        .bold(true)?
+        .writeln(&job.title)?
+        .feed()?
+        .feed()?
+        .reset_style_state()
+        .justify(JustifyMode::LEFT)?
+        .size(1, 1)?
+        .underline(UnderlineMode::None)?
+        .bold(false)?
+        .writeln(&job.description)?
+        .feed()?
+        .print_cut()?;
+
+    Ok(())
+}
+
+/// Task to do.
+#[derive(Serialize, Deserialize, ToSchema, Clone, Debug)]
+struct Todo {
+    /// Description of the tasks to do.
+    #[schema(example = "Washing Up")]
+    title: String,
+    #[schema(example = "Wash, dry and put all dishes away.")]
+    /// The text contents of the task
+    description: String,
+}
+
+
+/// Create new Todo to shared in-memory storage.
+///
+/// Post a new `Todo` in request body as json to print it. Api will return
+///
+/// One could call the api with.
+/// ```text
+/// curl localhost:8080/todo -d '{"title": "Do Washing up", "description": "Wash up the dishes"}'
+/// ```
+#[utoipa::path(
+    tag = TODO,
+    responses(
+        (status = 201, description = "Todo printed successfully", body = Todo),
+    )
+)]
+#[post("")]
+async fn create_todo(todo: Json<Todo>, todo_store: Data<TodoStore>) -> impl Responder {
+    let mut todos = todo_store.todos.lock().unwrap();
+    let todo = &todo.into_inner();
+    todos.push(todo.clone());
+    print(todo.clone());
+    HttpResponse::Created().json(todo)
+}
